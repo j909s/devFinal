fedac9533bad88f077b4187ba3636194fb41d559
tet <165059272+j909s@users.noreply.github.com>
2025-05-15 20:39:21 +0100

    tf files

---
+++ variables.tf
@@ -0,0 +1,23 @@
+## 
+# Variables
+##
+
+variable "location" {
+    type = string
+    default = "uksouth"
+}
+
+variable "naming_prefix" {
+  type    = string
+  default = "techielassgha"
+}
+
+variable "tag_environment" {
+  type    = string
+  default = "test"
+}
+
+variable "tag_owner" {
+  type    = string
+  default = "jade"
+}
